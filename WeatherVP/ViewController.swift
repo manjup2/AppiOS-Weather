76326f7689ec1c9413c9b20b51fa7af41269a3a2
Danyl Semmache <danyl.semmache@gmail.com>
2016-12-11 19:55:29 +0100

    Initial Commit

---
+++ WeatherVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherVP
+//
+//  Created by Danyl Semmache on 11/12/2016.
+//  Copyright © 2016 Danyl Semmache. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
